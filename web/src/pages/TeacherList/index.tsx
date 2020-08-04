2dc0810efb9dc410e98f045c0ac52d52c236512d
eduardo3g <eduardosbrasil10@gmail.com>
2020-08-03 22:42:13 -0300

    Created responsive teacher item component.

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
 
@@ -23,6 +24,13 @@ const TeacherList: React.FC = () => {
           </div>                    
         </form>
       </PageHeader>
+
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
     </div>
   );
 }
