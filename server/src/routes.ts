5bf2a0fe337bec2d93d792eec40e21ebe7e73807
eduardo3g <eduardosbrasil10@gmail.com>
2020-08-04 20:48:28 -0300

    feat: Listing connections.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
 routes.get('/healthcheck', (request, response) => {
   return response.json({ status: 'Alive' });
@@ -10,5 +12,8 @@ routes.get('/healthcheck', (request, response) => {
 
 routes.post('/classes', classesController.create);
 routes.get('/classes', classesController.index);
+
+routes.post('/connections', connectionsController.create);
+routes.get('/connections', connectionsController.index);
 
 export default routes;
