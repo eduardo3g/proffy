5c6be661dab063c05be5fd63011be087aaed8a12
eduardo3g <eduardosbrasil10@gmail.com>
2020-08-04 20:15:33 -0300

    chore: Error handling when creating new user.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -28,37 +28,45 @@ routes.post('/classes', async (request, response) => {
 
   const trx = await db.transaction();
 
-  const insertedUsersIds = await trx('users').insert({
-    name,
-    avatar,
-    whatsapp,
-    bio,
-  });
+  try {
+    const insertedUsersIds = await trx('users').insert({
+      name,
+      avatar,
+      whatsapp,
+      bio,
+    });
+  
+    const user_id = insertedUsersIds[0];
+  
+    const insertedClassesIds = await trx('classes').insert({
+      subject,
+      cost,
+      user_id,
+    });
+  
+    const class_id = insertedClassesIds[0];
+  
+    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+      return {
+        class_id,
+        week_day: scheduleItem.week_day,
+        from: convertHourToMinutes(scheduleItem.from),
+        to: convertHourToMinutes(scheduleItem.to),
+      }
+    });
+  
+    await trx('class_schedules').insert(classSchedule);
+  
+    await trx.commit();
+  
+    return response.status(201).send();
+  } catch (error) {
+    await trx.rollback();
 
-  const user_id = insertedUsersIds[0];
-
-  const insertedClassesIds = await trx('classes').insert({
-    subject,
-    cost,
-    user_id,
-  });
-
-  const class_id = insertedClassesIds[0];
-
-  const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-    return {
-      class_id,
-      week_day: scheduleItem.week_day,
-      from: convertHourToMinutes(scheduleItem.from),
-      to: convertHourToMinutes(scheduleItem.to),
-    }
-  });
-
-  await trx('class_schedules').insert(classSchedule);
-
-  await trx.commit();
-
-  return response.send();
+    return response.status(400).json({
+      error: 'Unexpected error while creating new class'
+    });
+  }
 });
 
 export default routes;
