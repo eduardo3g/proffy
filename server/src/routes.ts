8c97e6445d7a1bd36d7256c380eff21a2ab5d42d
eduardo3g <eduardosbrasil10@gmail.com>
2020-08-04 20:11:25 -0300

    feat: Creating class schedules w/ Knex transactions.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import db from './database/connection';
+
+import convertHourToMinutes from './utils/convertHourToMinutes';
+
 const routes = express.Router();
 
-import db from './database/connection';
+interface ScheduleItem {
+  week_day: number;
+  from: string;
+  to: string;
+}
 
 routes.get('/healthcheck', (request, response) => {
   return response.json({ status: 'Alive' });
@@ -18,12 +26,37 @@ routes.post('/classes', async (request, response) => {
     schedule,
   } = request.body;
 
-  await db('users').insert({
+  const trx = await db.transaction();
+
+  const insertedUsersIds = await trx('users').insert({
     name,
     avatar,
     whatsapp,
     bio,
   });
+
+  const user_id = insertedUsersIds[0];
+
+  const insertedClassesIds = await trx('classes').insert({
+    subject,
+    cost,
+    user_id,
+  });
+
+  const class_id = insertedClassesIds[0];
+
+  const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+    return {
+      class_id,
+      week_day: scheduleItem.week_day,
+      from: convertHourToMinutes(scheduleItem.from),
+      to: convertHourToMinutes(scheduleItem.to),
+    }
+  });
+
+  await trx('class_schedules').insert(classSchedule);
+
+  await trx.commit();
 
   return response.send();
 });
